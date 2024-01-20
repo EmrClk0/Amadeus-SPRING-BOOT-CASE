6e143cc80344c1e7eb63cd55734a8696661e4d90
EmrClk0 <83767839+EmrClk0@users.noreply.github.com>
2024-01-20 19:48:12 +0300

    proje açılıdı

---
+++ AmadeusAPP/src/main/java/com/Amadeus/AmadeusAppApplication.java
@@ -0,0 +1,13 @@
+package com.Amadeus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmadeusAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmadeusAppApplication.class, args);
+	}
+
+}
---
+++ AmadeusAPP/src/test/java/com/Amadeus/AmadeusAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Amadeus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmadeusAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
